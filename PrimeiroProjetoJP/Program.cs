41b23be7eb4d37a85e0142ea576535aa660cd800
wesleysemin <weseley.semin@gmail.com>
2025-05-07 20:09:23 -0300

    Mudei a Mensagen Idade

--- PrimeiroProjetoJP/Program.cs
+++ PrimeiroProjetoJP/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Meu Nome é Katia");
 
 Console.WriteLine("Meu Nome completo é Katia Schmitt Pollheim");
 
-Console.WriteLine("Tenho 31 anos.");
+Console.WriteLine("Tenho 21 anos.");
 
 Console.WriteLine("Oi Katia");
 
