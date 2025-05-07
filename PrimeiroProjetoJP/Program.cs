979caa1f4c0a514bdba7b0257194747b9faa00a3
wesleysemin <weseley.semin@gmail.com>
2025-05-07 20:55:36 -0300

    Inclusao de uma nova mensagem. exclução da mensagen idade.

--- PrimeiroProjetoJP/Program.cs
+++ PrimeiroProjetoJP/Program.cs
@@ -7,8 +7,6 @@ Console.WriteLine("Meu Nome completo é Katia Schmitt Pollheim");
 
 Console.WriteLine("Tenho 31 anos!");
 
-Console.WriteLine("Tenho 21 anos.");
-
 Console.WriteLine("Oi Katia");
 
 Console.WriteLine("Como você está");
@@ -16,3 +14,5 @@ Console.WriteLine("Como você está");
 Console.WriteLine("ta muito frio?");
 
 Console.WriteLine("Sim Wesley Está, desliguei o ar condicionado!");
+
+Console.WriteLine("Pode ligar denovo, quero ver eles tremerem aqui >:3");
