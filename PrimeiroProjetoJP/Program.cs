0a62a2f78e3d11f52e0aa29c90ecf28c92445d87
wesleysemin <weseley.semin@gmail.com>
2025-05-07 19:59:59 -0300

    Inclusao de  2 Novas Mensagens, Modificação de uma Mensagen

--- PrimeiroProjetoJP/Program.cs
+++ PrimeiroProjetoJP/Program.cs
@@ -7,4 +7,8 @@ Console.WriteLine("Meu Nome completo é Katia Schmitt Pollheim");
 
 Console.WriteLine("Tenho 31 anos");
 
-Console.WriteLine("Oi Wesley");
+Console.WriteLine("Oi Katia");
+
+Console.WriteLine("Como você está");
+
+Console.WriteLine("ta muito frio?");
